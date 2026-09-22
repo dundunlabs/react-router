52fceee08321107302fad025ede2b6ebafda0b3b
tdd28 <tdd2810@gmail.com>
2026-09-22 17:40:44 +0700

    chore: matchPath utils

--- src/utils/path.ts
+++ src/utils/path.ts
@@ -6,3 +6,14 @@ export function joinPaths(...paths: string[]) {
 
   return '/' + joinedPath
 }
+
+export function matchPath(path: string, pathname: string) {
+  const pattern = path
+    .replaceAll(/\/:[^/]+/g, key => `/(?<${key.slice(2)}>[^/]*)`)
+  const regex = new RegExp(`^${pattern}$`)
+
+  const match = pathname.match(regex)
+  if (!match) return null
+
+  return match.groups || {}
+}
--- tests/utils/path.test.ts
+++ tests/utils/path.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from "@rstest/core";
-import { joinPaths } from "../../src/utils/path";
+import { joinPaths, matchPath } from "../../src/utils/path";
 
 describe('path', () => {
   test.each([
@@ -25,5 +25,30 @@ describe('path', () => {
     }
   ])('joinPaths $segments', ({ segments, expected }) => {
     expect(joinPaths(...segments)).toBe(expected)
+  })
+
+  test.each([
+    {
+      path: '/',
+      pathname: '/foo',
+      expected: null
+    },
+    {
+      path: '/foo',
+      pathname: '/foo',
+      expected: {}
+    },
+    {
+      path: '/stories/:storyId',
+      pathname: '/stories/1',
+      expected: { storyId: "1" }
+    },
+    {
+      path: '/stories/:storyId/comments/:commentId',
+      pathname: '/stories/1/comments/2',
+      expected: { storyId: "1", commentId: "2" }
+    }
+  ])('matchPath $path with $pathname', ({ path, pathname, expected }) => {
+    expect(matchPath(path, pathname)).toEqual(expected)
   })
 })
